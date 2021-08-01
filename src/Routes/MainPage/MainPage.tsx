c661a1be6f0b1acc67553065c614048ad101cb60
ElGutiar <el12guitar@gmail.com>
2021-08-01 15:22:06 +0300

    Main page styles

--- src/Routes/MainPage/MainPage.tsx
+++ src/Routes/MainPage/MainPage.tsx
@@ -1,19 +1,46 @@
 import React from 'react'
+import { Button } from 'antd'
 
 import './mainPage.scss'
+import { NavLink } from 'react-router-dom'
 
 export default function MainPage() {
   return (
     <main className='main'>
-      <section>
-        <h1>
+      <section className='main__info container'>
+        <h1 className='main__info--title'>
           Soccer stat
         </h1>
 
-        <p>Watch all available leagues</p>
+        <p className='main__info--text'>
+          This is the best place for viewing statistics
+          of the leading European football tournaments
+        </p>
 
-        <button>Leagues</button>
+        <div className='main__info--buttons'>
+          <NavLink
+            to='/leagues'
+          >
+            <Button
+              style={{width: 100 + '%'}}
+              type='primary'
+            >
+              Watch Leagues
+            </Button>
+          </NavLink>
+
+          <NavLink to='/matches'>
+            <Button
+              style={{width: 100 + '%'}}
+              type='primary'
+            >
+              Watch today`s matches
+            </Button>
+          </NavLink>
+        </div>
       </section>
+      <div className='main__overlay'></div>
+      <div className='main__mask'></div>
     </main>
   )
 }
