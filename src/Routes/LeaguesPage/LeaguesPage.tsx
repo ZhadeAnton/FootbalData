4af959c5204f9ba82986c6c9a68d2d27020fef85
ElGutiar <el12guitar@gmail.com>
2021-08-01 15:55:18 +0300

    Leagues page paddings. Some styles for league item adaptive

--- src/Routes/LeaguesPage/LeaguesPage.tsx
+++ src/Routes/LeaguesPage/LeaguesPage.tsx
@@ -1,26 +1,32 @@
 import React from 'react'
+import { Divider } from 'antd'
 
 import './leaguesPage.scss'
 import { ILeaguesContainer } from '../../Containers/LeaguesContainer'
+import CompetitionHeader from '../../Components/CompetitionHeader/CompetitionHeader'
+import BackTopButton from '../../Components/BackButton/BackButton'
 import LeaguesList from '../../Components/Leagues/LeaugesList/LeaguesList'
-import BrandHeading from '../../Components/BrandHeading/BrandHeading'
 import SortInput from '../../Components/FilterInput/SortInput'
 
 export default function LeaguesPage(props: ILeaguesContainer) {
   return (
     <main className='leagues'>
       <section className='leagues__wrapper container'>
-        <BrandHeading style={{marginBottom: 2 + 'rem'}}>
-          List of available leagues
-        </BrandHeading>
+        <CompetitionHeader competitionName='List of available league'/>
+
+        <Divider />
 
         <SortInput />
+
+        <Divider />
 
         <LeaguesList
           leagues={props.leagues}
           handleClickByLeague={props.handleClickByLeague}
         />
       </section>
+
+      <BackTopButton />
     </main>
   )
 }
